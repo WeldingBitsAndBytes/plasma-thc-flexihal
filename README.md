1c25be3cfdf12e21eec24c9cb8de8a005c18f19c
WeldingBitsAndBytes <WeldingBitsAndBytes>
2025-01-27 20:51:01 -0500

    WIP

---
+++ README.md
@@ -0,0 +1,70 @@
+# WIP
+
+A THC control board that can connect to a Expatria Flexihal for controlling a plasma cutter
+
+I've highlighted BOMs for each major component, but I've left some things like hookup wires,
+pins, etc to your own design and in particular your own plasma cutter.
+
+## THC Divider Board
+
+`thc_divider_board` directory contains a Kicad PCB designed to isolate the raw plasma cutter 
+measurements from the MCU. The PCBs were designed with Oshpark in mind, in particular the
+"After Dark" style just for fun. As such, there is minimal silkscreening.
+
+![front](thc_divider_board/front.png)
+
+![front](thc_divider_board/back.png)
+
+The board is intended to be powered by an internal 24VDC power supply within the plasma cutter. The 
+plasma cutter's power supply is protected by a 6KVDC reinforced DC-DC converter. The low-side of the 
+isolation amplifier is powered by another isolation DC-DC converter fed from the main DC-DC 
+converter. This secondary 5V DC-DC converter also provides power for the MCU.
+
+The MCU is intended to be something like an Arduino Nano. Almost any other MCU would work but these
+are cheap, small, and just fine for this application.
+
+## Nano Voltage Sender
+
+`nano_voltage_sender` contains the sketch for the Arduino Nano. It uses an external Vref voltage source
+to normalize out the voltage for more accurate readings. It uses the 2V reference source to match the
+0-2V output from the THC divider board.
+
+The signal is connected to the ADC. The MCU is connected to a https://www.dfrobot.com/product-2588.html 
+UART Fibre Optic Transceiver
+
+BOM:
+
+| Part    | Quantity | Notes |
+| -------- | ------- | ----- |
+| Arduino Nano | 1 | Or similar |
+| UART Fiber Optic Transceiver | 1 | https://www.dfrobot.com/product-2588.html |
+| LM4040 Voltage Reference | 1 | Or similar - eg: https://www.adafruit.com/product/2200 |
+| Arduino Nano Screw Terminal Adapter | 1 | https://store-usa.arduino.cc/products/nano-screw-terminal is really nice |
+| SC Fibre Optic Cable | 1 | Recommend duplex so you can fire a relay for plasma control |
+
+## Teensy Quadrature Encoder
+
+The `teensy_quadrature_encoder` directory contains the sketch for a Teensy 4 to convert from
+serial to quadrature encoding. The Teensy 4 was chosen for its high speed and ability to measure nanoseconds 
+for precise timings.  The quadrature encoding is then used to interface with a Expatria FlexiHAL and by 
+extension LinuxCNC for torch height control. Similarly, this Teensy will also have a UART Fibre Optic 
+Transceiver to talk to the Nano
+
+BOM:
+
+| Part    | Quantity | Notes |
+| -------- | ------- | ----- |
+| Teensy 4 | 1 | |
+| UART Fiber Optic Transceiver | 1 | https://www.dfrobot.com/product-2588.html |
+| Screw terminals | 8 | Have enough screw terminals for the uart serial, and power output, and quadrature encoder output |
+
+## FlexiHAL Hookup and Configuration
+
+BOM:
+
+| Part    | Quantity | Notes |
+| -------- | ------- | ----- |
+| Ethernet cable, TIA 568B straight-through | 1 | |
+| Flexihal! | 1 | Duh |
+
+More to come :)
---
+++ teensy_quadrature_encoder/teensy_quadrature_encoder.ino
@@ -0,0 +1,224 @@
+
+
+// Quadrature Generator
+// https://wokwi.com/projects/403218793081155585
+
+// Define the output pins
+
+
+
+#include "Arduino.h"
+
+namespace cycles64
+{
+    uint64_t get();
+
+    namespace // private -----------------------------
+    {
+        uint32_t oldLow = ARM_DWT_CYCCNT;
+        uint32_t curHigh = 0;
+
+        void SNVS_isr(void)
+        {
+            SNVS_HPSR |= 0b11; // reset interrupt flag
+            get();             // call to check for overflow
+            asm("dsb");        // prevent double calls of the isr
+        }
+
+    } // end private namespace <<---------------------
+
+    uint64_t get()
+    {
+        noInterrupts();
+        uint32_t curLow = ARM_DWT_CYCCNT;
+        if (curLow < oldLow) // roll over
+        {
+            curHigh++;
+        }
+        oldLow = curLow;
+        uint64_t curVal = ((uint64_t)curHigh << 32) | curLow;
+        interrupts();
+
+        return curVal;
+        
+    }
+
+    void begin()
+    {
+        // disable periodic snvs interrupt
+        SNVS_HPCR &= ~SNVS_HPCR_PI_EN;
+        while ((SNVS_HPCR & SNVS_HPCR_PI_EN)) {}
+
+        // interrupt frequency 1Hz
+        SNVS_HPCR = SNVS_HPCR_PI_FREQ(0b1111);
+
+        // enable periodic snvs interrupt
+        SNVS_HPCR |= SNVS_HPCR_PI_EN;
+        while (!(SNVS_HPCR & SNVS_HPCR_PI_EN)) {}
+
+        attachInterruptVector(IRQ_SNVS_IRQ, SNVS_isr);
+        NVIC_SET_PRIORITY(IRQ_SNVS_IRQ, 255); // lowest
+        NVIC_ENABLE_IRQ(IRQ_SNVS_IRQ);
+    }
+}
+
+
+
+
+#define pinApos 3
+#define pinAneg 4
+#define pinBpos 10
+#define pinBneg 11
+
+// Define the state machine states
+enum State {STATE0, STATE1, STATE2, STATE3};
+State currentState = STATE0;
+
+// Timing variables
+uint64_t previousNanos = 0;
+float interval = 1000.0; // 1000 nanoeconds for each state
+
+
+int content;
+int old = "";
+const byte numBytes = 32;
+byte receivedBytes[numBytes];
+byte numReceived = 0;
+boolean newData = false;
+
+
+void setup() {
+  Serial.begin(8000000);
+  //while (!Serial);
+
+  // Initialize the output pins
+  pinMode(pinApos, OUTPUT);
+  pinMode(pinAneg, OUTPUT);
+  pinMode(pinBpos, OUTPUT);
+  pinMode(pinBneg, OUTPUT);
+
+  pinMode(13, OUTPUT);
+
+  // Initialize the pins to a known state
+  digitalWrite(pinApos, LOW);
+  digitalWrite(pinAneg, LOW);
+  digitalWrite(pinBpos, LOW);
+  digitalWrite(pinBneg, LOW);
+
+  digitalWrite(13, HIGH);
+
+  cycles64::begin();
+
+  Serial1.begin(1000000);
+  while (!Serial1);
+}
+
+void loop() {
+  recvBytesWithStartEndMarkers();
+  parseNewData();
+  quadGenerator();
+}
+
+void recvBytesWithStartEndMarkers() {
+  /*
+  profiler_t profiler(
+    (String("Time spent in ") + 
+    String(__FUNCTION__) + 
+    String("()")).c_str());
+  */
+
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    byte startMarker = 0x3C;
+    byte endMarker = 0x3E;
+    byte rb;
+   
+    while (Serial1.available() > 0 && newData == false) {
+        rb = Serial1.read();
+        if (recvInProgress == true) {
+            if (rb != endMarker) {
+                receivedBytes[ndx] = rb;
+                ndx++;
+                if (ndx >= numBytes) {
+                    ndx = numBytes - 1;
+                }
+            }
+            else {
+                receivedBytes[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                numReceived = ndx;  // save the number for use when printing
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rb == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseNewData() {
+  /*
+  profiler_t profiler(
+    (String("Time spent in ") + 
+    String(__FUNCTION__) + 
+    String("()")).c_str());
+  */
+  if (newData == true) {
+    newData = false;
+    int content = (receivedBytes[0]<<8) + receivedBytes[1];
+    if (content != old) {
+      old = content;
+      unsigned int frequency = (content * 100) + 10000; // Range from 10khz to 11.23khz
+      interval = (1000000.0 / frequency / 4) * 1000.0; // Convert frequency to interval and divide by 4 as we need to do 4 things in a cycle 
+      Serial.print(interval);
+      Serial.print(" - ");
+      Serial.println(frequency);
+    }
+  }
+}
+
+void quadGenerator(){
+  /*
+  profiler_t profiler(
+    (String("Time spent in ") + 
+    String(__FUNCTION__) + 
+    String("()")).c_str());
+  */
+  // Get the current time in nanoseconds
+  // Check if the interval has passed
+
+  if (cycles64::get() - previousNanos >= interval) {
+
+    // Advance the state machine
+    switch (currentState) {
+      case STATE0:
+        digitalWrite(pinApos, HIGH);
+        digitalWrite(pinAneg, LOW);
+        currentState = STATE1;
+        break;
+
+      case STATE1:
+        digitalWrite(pinBpos, HIGH);
+        digitalWrite(pinBneg, LOW);
+        currentState = STATE2;
+        break;
+
+      case STATE2:
+        digitalWrite(pinApos, LOW);
+        digitalWrite(pinAneg, HIGH);
+        currentState = STATE3;
+        break;
+
+      case STATE3:
+        digitalWrite(pinBpos, LOW);  
+        digitalWrite(pinBneg, HIGH);
+        currentState = STATE0;
+        break;       
+    }
+    
+    previousNanos = cycles64::get();
+
+  }
+}
